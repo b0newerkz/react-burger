c25ae4d30ea0e66bec27fbc7fa2a5d181c5823a9
fest <thefest29@gmail.com>
2024-03-09 21:39:22 +0300

    initial step 2

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -8,7 +8,46 @@ import { ApiContext, ConstructorContext } from '../../utils/context';
 import { v4 as uuidv4 } from 'uuid';
 
 const API_URL = 'https://norma.nomoreparties.space/api/ingredients';
+/*
+	import { compose, createStore, applyMiddleware } from 'redux';
+	import thunk from 'redux-thunk';
 
+	const composeEnhancers =
+	typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+		: compose;
+
+	const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+	const store = createStore(rootReducer, enhancer);
+
+	или
+
+	const initialState = {todos: []}
+	const store = createStore(
+		rootReducer,
+		initialState,
+		compositeWithDevTools(applyMiddleWare(...midleware))
+	); 
+
+	REDUX TOOLKIT
+	import { configureStore } from '@reduxjs/toolkit'
+	import logger from 'redux-logger'
+	import todosReducer from './todos/todosReducer'
+	import { customEnhancer } from './enhancers'
+
+	const preloadedState = {
+	todos: [],
+	}
+	const store = configureStore({
+	reducer,
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+	devTools: process.env.NODE_ENV !== 'production',
+	preloadedState,
+	enhancers: [customEnhancer],
+	}) 
+
+*/
 const initialIngredients = {
 	
 	sum: 0,
@@ -73,7 +112,7 @@ const App = () => {
 
 	React.useEffect(() => {
 
-		request(API_URL).then(data => {
+		/*request(API_URL).then(data => {
 
 			if(data.success) {
 				setData({isLoaded: true, hasError: false, ingredients: data.data})
@@ -83,7 +122,7 @@ const App = () => {
 		}).catch(e => {
 			console.log("Ошибка: "+ e)
 			setData({...data.ingredients, isLoading: false, hasError: true })
-		})
+		})*/
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
@@ -92,12 +131,12 @@ const App = () => {
 			<AppHeader />
 			
 			<main className={style.container}>
-				<ApiContext.Provider value={data.ingredients}>
-					<ConstructorContext.Provider value={{ingredients, dispatchIngredients}}>
+				{/*<ApiContext.Provider value={data.ingredients}>
+					<ConstructorContext.Provider value={{ingredients, dispatchIngredients}}>*/}
 						{data.isLoaded && !data.hasError && <BurgerIngredients />}
 						{data.isLoaded && !data.hasError && <BurgerConstructor />}
-					</ConstructorContext.Provider>
-				</ApiContext.Provider>
+					{/*</ConstructorContext.Provider>
+				</ApiContext.Provider>*/}
 			</main>
 		</>
 	)
